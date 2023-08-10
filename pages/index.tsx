8f87d9d61814360ef3c81fa91471769efac21042
Sib <chengjie.michael.huang@gmail.com>
2023-08-10 00:06:33 -0700

    refactored index pg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,24 @@
-import fs from "fs";
-import Head from "next/head";
-import { Carousel } from "react-responsive-carousel";
-import path from "path";
 import RowContainer from "../components/rowContainer";
 import { getPostContent } from "../helpers/posts";
 
 export default function Home(props) {
-  console.log(props);
   return (
     <div>
       <RowContainer
         title="Latest Adventures"
+        dir="adventure"
         posts={props.adventurePosts}
         seeMore="/Adventures"
       />
       <RowContainer
         title="Latest Projects"
+        dir="showcase"
         posts={props.showcasePosts}
         seeMore="/Projects"
       />
     </div>
   );
 }
-
-
 
 export async function getStaticProps() {
   const adventurePosts = await getPostContent("adventure");

847f621e760ad03a683e75c993cb451ec48f9f2f
Sib <chengjie.michael.huang@gmail.com>
2023-08-16 00:14:21 -0700

    restyled component to be more dynamically sized

--- components/PostRow.tsx
+++ components/PostRow.tsx
@@ -4,26 +4,26 @@ export default function RowContainer(props: any) {
   const showSeeMore = props.posts.length > 3 && false;
 
   return (
-    <div className="pb-4">
+    <div className="pb-4 h-full w-full">
       <div className="text-3xl">{props.title}</div>
-      <div className="flex flex-row gap-x-20 items-center justify-center space-x-10">
+      <div className="flex flex-row gap-x-20 items-center justify-center space-x-10 h-full p-4">
         {props.posts.slice(0, 3).map((article: any, index: number) => (
-          <div key={index}>
+          <div className="h-full" key={index}>
             <PostItem post={article} />
           </div>
         ))}
       </div>
-      {showSeeMore && (
+      {/* {showSeeMore && (
         <div className="text-right pr-4 font-semibold">
           <button onClick={() => log(props.seeMore)}>
             <a>See More</a>
           </button>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
 
-function log(item: any) {
-  console.log(item);
-}
+// function log(item: any) {
+//   console.log(item);
+// }
--- components/postItem.tsx
+++ components/postItem.tsx
@@ -1,36 +1,42 @@
 import Image from "next/image";
+import {alt_caption} from "../helpers/posts";
 
 export default function PostItem({ post }) {
   const hasImg = post.frontmatter.thumbnailUrl;
   return (
-    <a href={post.slug}>
-      <div className="grid grid-cols-3 rounded-xl border-2 mx-8 my-2 h-86">
-        {hasImg && (
-          <div className="col-span-3 w-80 h-64 relative m-auto">
-            <Image
-              src={post.frontmatter.thumbnailUrl}
-              style={{
-                objectFit: "contain",
-              }}
-              fill
-              alt="img"
-              sizes="100%"
-              className="p-2"
-            />
+    <a href={post.slug} className="h-max">
+      <div className="h-full flex flex-col justify-center rounded-xl border-2">
+        {/* Image Container */}
+        <div className="h-full">
+          {hasImg && (
+            <div className="col-span-3 relative m-auto min-h-full rounded-xl">
+              <Image
+                src={post.frontmatter.thumbnailUrl}
+                style={{
+                  objectFit: "contain",
+                }}
+                fill
+                alt={alt_caption(post.frontmatter.thumbnailUrl)}
+                className="p-2 rounded-xl"
+              />
+            </div>
+          )}
+        </div>
+        {/* Text Container */}
+        <div className="grid grid-cols-3">
+          {!hasImg && <div className="col-span-3" />}
+          <div className="col-span-2 text-xl font-semibold pl-2">
+            {post.frontmatter.title}
           </div>
-        )}
-        {!hasImg && <div className="h-64 col-span-3" />}
-        <div className="col-span-2 text-xl font-semibold pl-2">
-          {post.frontmatter.title}
+          <div className="col-span-1 text-right pr-2">
+            {post.frontmatter.date}
+          </div>
+          <div className="col-span-3 px-2">{post.frontmatter.description}</div>
+          <div className="col-span-3 w-80 px-2 line-clamp-2">
+            <div>{renderTags(post.frontmatter.tags)}</div>
+          </div>
+          <div />
         </div>
-        <div className="col-span-1 text-right pr-2">
-          {post.frontmatter.date}
-        </div>
-        <div className="col-span-3 px-2">{post.frontmatter.description}</div>
-        <div className="col-span-3 w-80 px-2 line-clamp-2">
-          <div>{renderTags(post.frontmatter.tags)}</div>
-        </div>
-        <div />
       </div>
     </a>
   );
